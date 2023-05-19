e7a1af9d275190c90d655c8e0904729585a9aba5
Crystal <99391710+x200706@users.noreply.github.com>
2023-05-19 14:54:22 +0800

    update Mouse.java

--- src/main/java/org/example/Mouse.java
+++ src/main/java/org/example/Mouse.java
@@ -14,6 +14,8 @@ public class Mouse {
                 {2, 0, 0, 0, 0, 0, 2},
                 {2, 2, 2, 2, 2, 0, 2},
         };
+        //讓老鼠走迷宮，經過的可行路線值改為1
+
         //print出路徑，道路是空字串，牆壁是█，勞贖是◇
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
@@ -24,10 +26,15 @@ public class Mouse {
                 } else if (maze[i][j] == 0 && j != 6) {
                     System.out.print(" ");
                 } else if (maze[i][j] == 0 && j == 6) {
-                    System.out.println(" ");//目前為止只印出了迷宮
+                    System.out.println(" ");
+                } else if (maze[i][j] == 1) {
+                    System.out.print("◇");
                 }
 
             }
         }
+    }
+    public boolean mouseGo(){
+        return false;
     }
 }
