b9dd05427483bbc3011fb785801d043dfe3bf1f6
Crystal <chi200706@gmail.com>
2023-05-20 13:36:11 +0800

    update Mouse.java

--- src/main/java/org/example/Mouse.java
+++ src/main/java/org/example/Mouse.java
@@ -12,10 +12,11 @@ public class Mouse {
             {2, 2, 2, 2, 2, 0, 2},
     };
 
+    static boolean flag = false; //全場唯一的旗子
 
     public static void main(String[] args) {
-        if (!mouseGo(1, 0)) {//傳入本題勞贖的起點
-            System.out.println("此迷宮無解");
+        if (!mouseGo(1, 0)) { //"不"跟false負負的正為true，無解就進這段
+            System.out.println("這個迷宮沒有出口");
         } else {
             //print出路徑，道路是空字串，牆壁是█，勞贖是◇
             for (int i = 0; i < maze.length; i++) {
@@ -33,32 +34,32 @@ public class Mouse {
         }
     }
 
-    static boolean flag = false;
-
-    public static boolean mouseGo(int i, int j) { //讓老鼠走迷宮，經過的可行路線值改為1
+    public static boolean mouseGo(int i, int j) { //讓勞贖走迷宮，經過的可行路線值改為1
         //勞贖走過的地方，標記為1
         maze[i][j] = 1;
 
         //是否走完迷宮的註記
         if (i == 6 && j == 5) { //本題的出口為maze[6][5]
-            return true;
+            flag = true;
         }
 
         //勞贖開始探索四個方位如果迷宮還沒走完，而且勞贖發現右邊為0即為有路
         if (!flag && j + 1 < maze[0].length && maze[i][j + 1] == 0) {
             //繼續走迷宮（遞迴）
             mouseGo(i, j + 1);//老鼠往右移一格
-        } else if (!flag && i + 1 < maze.length && maze[i + 1][j] == 0) { //勞贖途中嘗試往下走的情況..
+        }
+        if (!flag && i + 1 < maze.length && maze[i + 1][j] == 0) { //勞贖途中嘗試往下走的情況..
             mouseGo(i + 1, j);
-        } else if (!flag && i - 1 >= 0 && maze[i - 1][j] == 0) { //勞贖途中嘗試往上走的情況..
+        }
+        if (!flag && i - 1 >= 0 && maze[i - 1][j] == 0) { //勞贖途中嘗試往上走的情況..
             mouseGo(i - 1, j);
-        } else if (!flag && j - 1 >= 0 && maze[i][j - 1] == 0) { //勞贖途中嘗試往左走的情況..
+        }
+        if (!flag && j - 1 >= 0 && maze[i][j - 1] == 0) { //勞贖途中嘗試往左走的情況..
             mouseGo(i, j - 1);
         }
-
-//        if (!flag) { //勞贖嘗試過無效退掉的道路，拔除路徑標記，恢復為道路0
-//            maze[i][j] = 0;
-//        }
+        if (!flag) { //勞贖嘗試過無效退掉的道路，拔除路徑標記，恢復為道路0
+            maze[i][j] = 0;
+        }
         return flag;
     }
 }
