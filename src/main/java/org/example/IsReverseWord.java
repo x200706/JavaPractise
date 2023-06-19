592221c242b1d0bfa82f9eca9af484a5974e00dc
Crystal <99391710+x200706@users.noreply.github.com>
2023-06-19 17:44:55 +0800

    add a file for quiz

---
+++ src/main/java/org/example/IsReverseWord.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class IsReverseWord {
+    public static String[] words = {"RADAR", "WARTER START", "MILK KLIM", "RESERVERED", "IWI"};
+
+    public void test() {
+        //遍歷words
+        //切字串，放入Deque 雙向佇列
+        //比對從尾端取出是否等於從頭取出，是的話就放進答案陣列
+        //print出答案陣列
+    }
+}
