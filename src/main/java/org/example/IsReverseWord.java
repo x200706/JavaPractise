6464d08efbc5a8a94d4b2910124f1876f4f357a6
Crystal <99391710+x200706@users.noreply.github.com>
2023-06-20 15:08:57 +0800

    upload a file for quiz

--- src/main/java/org/example/IsReverseWord.java
+++ src/main/java/org/example/IsReverseWord.java
@@ -1,12 +1,33 @@
 package org.example;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+
 public class IsReverseWord {
     public static String[] words = {"RADAR", "WARTER START", "MILK KLIM", "RESERVERED", "IWI"};
 
     public void test() {
+        //宣告解答List
+        ArrayList<String> ansList = new ArrayList<>();
+
         //遍歷words
-        //切字串，放入Deque 雙向佇列
-        //比對從尾端取出是否等於從頭取出，是的話就放進答案陣列
+        for (String word : words) {
+            //切字串，放入Deque 雙向佇列
+            char[] checkArr = word.toCharArray();
+            Deque<Character> checkDeque = new ArrayDeque<>();
+            for (char element : checkArr) {
+                checkDeque.add(element);
+            }
+
+            //比對從尾端取出是否等於從頭取出，是的話就放進答案陣列
+//            if(相等){
+//                ansList.add(word);
+//            }
+        }
+
         //print出答案陣列
+        System.out.println(Arrays.toString(ansList.toArray()));
     }
 }
